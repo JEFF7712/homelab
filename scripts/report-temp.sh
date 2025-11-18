33de661bbc7caa993119452a6e59b96fefc9f2a9
Rupan Sunderapandyan <rupanpandyan@gmail.com>
2025-11-17 23:47:02 -0600

    Fixes

--- scripts/report-temp.sh
+++ scripts/report-temp.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 TEMP=$(sensors | awk '/Package id 0/ {gsub(/\+/,"",$4); gsub(/° C/,"",$4); print int($4); exit}')
 
 DATE_STR=$(date +"%Y-%m-%d %H:%M:%S")
 
 echo "[$DATE_STR] Current server temperature: $TEMP °C"
+
+## ADD THIS IN crontab -e
+
+#0 0,12 * * * TEMP=$(/usr/local/bin/report-temp.sh) && curl -s -X POST "https://api.telegram.org/<bot-API>/sendMessage" -d chat_id=<chat-id> -d text="$TEMP"
--- scripts/test-watchdog.sh
+++ scripts/test-watchdog.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
+ENV_FILE="/home/rupan/homelab/.env"
+if [ -f "$ENV_FILE" ]; then
+  set -a
+  . "$ENV_FILE"
+  set +a
+else
+  echo "Warning: $ENV_FILE not found, using existing environment variables"
+fi
 
 MAX=20
-ALERT_CMD="curl -s -X POST https://api.telegram.org/bot8566657112:AAFifQmpm32_Z4ZnpU_K-WfgRAqYnRcQvcE/sendMessage -d chat_id=7542293680 -d text='WARNING: Server overheating! Shutting down now.'"
+ALERT_CMD="curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text='WARNING: Server overheating! Shutting down now.'"
 
 while true; do
     TEMP_RAW=$(sensors | grep 'Package id 0' | awk '{print $4}' | tr -d '+°C')
