051219968ea967c7f8d95705418023ee19cd5572
JEFF7712 <rupanpandyan@gmail.com>
2025-12-24 22:53:22 -0600

    refactor(infra): reorganize flux and terraform setup
    Consolidate Flux Kustomizations for infrastructure components.
    Restructure MetalLB and Traefik deployments into dedicated subdirectories.
    Refactor Terraform DHCP reservations to use a `for_each` loop.
    Add HelmRepository for podinfo application and update MetalLB IP pool range.

--- terraform/infrastructure/network-mappings.tf
+++ terraform/infrastructure/network-mappings.tf
@@ -1,94 +1,58 @@
+locals {
+  subnet_map = {
+    lan     = opnsense_kea_subnet.lan.id
+    cluster = opnsense_kea_subnet.cluster.id
+  }
+
+  reservations = {
+    hp_mini = {
+      subnet = "lan", ip = "192.168.1.10", mac = "18:60:24:28:7d:1a", desc = "HP Mini"
+    }
+    chromebook = {
+      subnet = "lan", ip = "192.168.1.70", mac = "0c:37:96:b2:a6:1f", desc = "Chromebook"
+    }
+    tp_switch = {
+      subnet = "lan", ip = "192.168.1.2", mac = "bc:07:1d:2e:79:82", desc = "TP Link Switch"
+    }
+    netbird_lxc = {
+      subnet = "lan", ip = var.netbird_lxc_ip_addr, mac = var.netbird_lxc_mac_address, desc = "Netbird LXC"
+    }
+    talos_cp_01 = {
+      subnet = "cluster", ip = var.talos_cp_01_ip_addr, mac = var.talos_vm_mac_address, desc = "Talos Control Plane 1"
+    }
+    talos_worker_01 = {
+      subnet = "cluster", ip = var.talos_worker_01_ip_addr, mac = var.cli01_mac_address, desc = "Talos Worker 1"
+    }
+    talos_worker_02 = {
+      subnet = "cluster", ip = var.talos_worker_02_ip_addr, mac = var.cli02_mac_address, desc = "Talos Worker 2"
+    }
+    talos_worker_03 = {
+      subnet = "cluster", ip = var.talos_worker_03_ip_addr, mac = var.wyse_mac_address, desc = "Talos Worker 3"
+    }
+  }
+}
+
 resource "opnsense_kea_subnet" "lan" {
-  subnet = "192.168.1.0/24"
+  subnet          = "192.168.1.0/24"
   match_client_id = false
-    pools = [
-    "192.168.1.100-192.168.1.254"
-  ]
-  description = "LAN"
+  pools           = ["192.168.1.100-192.168.1.254"]
+  description     = "LAN"
 }
 
 resource "opnsense_kea_subnet" "cluster" {
-  subnet = "10.0.20.0/24"
+  subnet          = "10.0.20.0/24"
   match_client_id = false
-    pools = [
-    "10.0.20.100-10.0.20.200"
-  ]
-  routers = [
-    "10.0.20.1"
-  ]
-  dns_servers = [
-    "10.0.20.1",
-    "8.8.8.8",
-    "1.1.1.1"
-  ]
-  description = "CLUSTER"
+  pools           = ["10.0.20.100-10.0.20.179"]
+  routers         = ["10.0.20.1"]
+  dns_servers     = ["10.0.20.1", "8.8.8.8", "1.1.1.1"]
+  description     = "CLUSTER"
 }
 
-resource "opnsense_kea_reservation" "hp_mini" {
-  subnet_id    = opnsense_kea_subnet.lan.id
-  ip_address   = "192.168.1.10"
-  mac_address  = "18:60:24:28:7d:1a"
-  description  = "HP Mini"
-}
+resource "opnsense_kea_reservation" "this" {
+  for_each = local.reservations
 
-resource "opnsense_kea_reservation" "chromebook" {
-  subnet_id    = opnsense_kea_subnet.lan.id
-  ip_address   = "192.168.1.70"
-  mac_address  = "0c:37:96:b2:a6:1f"
-  description  = "Chromebook"
-}
-
-resource "opnsense_kea_reservation" "tp_switch" {
-  subnet_id    = opnsense_kea_subnet.lan.id
-  ip_address   = "192.168.1.2"
-  mac_address  = "bc:07:1d:2e:79:82"
-  description  = "TP Link Switch"
-}
-
-resource "opnsense_kea_reservation" "netbird_lxc" {
-  subnet_id    = opnsense_kea_subnet.lan.id
-  ip_address   = var.netbird_lxc_ip_addr
-  mac_address  = var.netbird_lxc_mac_address
-  description  = "Netbird LXC"
-}
-
-resource "opnsense_kea_reservation" "talos_controlplane_01" {
-  subnet_id    = opnsense_kea_subnet.cluster.id
-  ip_address   = var.talos_cp_01_ip_addr
-  mac_address  = var.talos_vm_mac_address
-  description  = "Talos Control Plane 1"
-}
-
-# resource "opnsense_kea_reservation" "talos_controlplane_02" {
-#   subnet_id    = opnsense_kea_subnet.cluster.id
-#   ip_address   = var.talos_cp_02_ip_addr
-#   mac_address  = "00:50:ac:93:01:b0"
-#   description  = "Talos Control Plane 2"
-# }
-
-# resource "opnsense_kea_reservation" "talos_controlplane_03" {
-#   subnet_id    = opnsense_kea_subnet.cluster.id
-#   ip_address   = var.talos_cp_03_ip_addr
-#   mac_address  = "00:50:ac:93:02:d6"
-#   description  = "Talos Control Plane 3"
-# }
-resource "opnsense_kea_reservation" "talos_worker_01" {
-  subnet_id    = opnsense_kea_subnet.cluster.id
-  ip_address   = var.talos_worker_01_ip_addr
-  mac_address  = var.cli01_mac_address
-  description  = "Talos Worker 1"
-}
-
-resource "opnsense_kea_reservation" "talos_worker_02" {
-  subnet_id    = opnsense_kea_subnet.cluster.id
-  ip_address   = var.talos_worker_02_ip_addr
-  mac_address  = var.cli02_mac_address
-  description  = "Talos Worker 2"
-}
-
-resource "opnsense_kea_reservation" "talos_worker_03" {
-  subnet_id    = opnsense_kea_subnet.cluster.id
-  ip_address   = var.talos_worker_03_ip_addr
-  mac_address  = var.wyse_mac_address
-  description  = "Talos Worker 3"
+  subnet_id   = local.subnet_map[each.value.subnet]
+  ip_address  = each.value.ip
+  mac_address = each.value.mac
+  description = each.value.desc
 }
