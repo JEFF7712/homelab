76a61a7b343fec71bd533100def98ac0e81d0890
JEFF7712 <rupanpandyan@gmail.com>
2026-01-29 10:34:16 -0600

    feat(moltbot): add deployment configurations, network policies, and external secrets for moltbot

---
+++ infrastructure/moltinator/flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    moltbot.url = "github:moltbot/nix-moltbot";
+  };
+
+  outputs = { self, nixpkgs, moltbot, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    
+    myMoltbot = moltbot.packages.${system}.default;
+  in {
+    packages.${system}.dockerImage = pkgs.dockerTools.buildLayeredImage {
+      name = "moltbot-hive-node";
+      tag = "latest";
+      
+      contents = [ 
+        myMoltbot
+        pkgs.cacert
+        pkgs.iana-etc
+        pkgs.tzdata
+        pkgs.bash
+        pkgs.coreutils
+      ];
+
+      extraCommands = ''
+        mkdir -m 1777 tmp
+        mkdir -m 755 data
+      '';
+
+      config = {
+        Cmd = [ "${myMoltbot}/bin/moltbot-gateway" ];
+        
+        Env = [
+          "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+          "HOME=/data"
+        ];
+        WorkingDir = "/data";
+        Volumes = { "/data" = {}; };
+      };
+    };
+  };
+}
